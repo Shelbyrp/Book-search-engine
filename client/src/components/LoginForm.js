98c01b5525e39f04bbdaf130a6d604f1c5c33320
Shelbyrp <77222563+Shelbyrp@user.noreply.github.com>
2021-08-01 14:40:59 +0800

    Update the login form.

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,17 +1,15 @@
 // see SignupForm.js for comments
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
-// import { loginUser } from '../utils/API';
-import Auth from '../utils/auth';
-import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../utils/mutations';
+import { useMutation } from '@apollo/react-hooks';
+import Auth from '../utils/auth';
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
-  const [login, { error, data }] = useMutation(LOGIN_USER);
+  const [login, { error }] = useMutation(LOGIN_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -30,7 +28,7 @@ const LoginForm = () => {
 
     try {
       const { data } = await login({
-        variables: { ...userFormData},
+        variables: { ...userFormData }
       });
 
       Auth.login(data.login.token);
@@ -40,7 +38,6 @@ const LoginForm = () => {
     }
 
     setUserFormData({
-      username: '',
       email: '',
       password: '',
     });
